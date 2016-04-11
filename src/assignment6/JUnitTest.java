9982f6ad9653b35dfec3a1b877cac776d7f8b2a7
kevinjyee <kevjyee@gmail.com>
2016-04-11 14:05:27 -0500

    first commit

---
+++ src/assignment6/JUnitTest.java
@@ -0,0 +1,14 @@
+package assignment6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
