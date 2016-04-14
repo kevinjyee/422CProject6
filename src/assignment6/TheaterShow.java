1e8a15bf914dcca858c37edc70d4bc1124aee301
kevinjyee <kevjyee@gmail.com>
2016-04-14 09:33:40 -0500

    Added Seat Init

--- src/assignment6/TheaterShow.java
+++ src/assignment6/TheaterShow.java
@@ -1,6 +1,8 @@
 package assignment6;
 
 import java.util.*;
+
+import assignment6.Seat.Section;
 public class TheaterShow {
 	
 	/*
@@ -9,10 +11,33 @@ public class TheaterShow {
 	
 	protected Queue<Seat> theatreSeats;
 	
+	public final ArrayList<String> rowArray = new ArrayList<String>();
 	
-	private void init(){
+	
+	
+	private void initRows(){
+		
+		for(int i =0; i<28; i++){
+			
+			char character = 'A';
+			rowArray.add(character++ + "");
+			
+		}
+		
+		
+	}
+	
+	private void initSeats(){
 		
 		theatreSeats = new LinkedList<Seat>();
+		
+		
+		/*Change this method later to make a more accurate representation of BATES HALL*/
+		
+		for(int seatNumber = 101; seatNumber <= 128; seatNumber++){
+			theatreSeats.add(new Seat(Section.HouseLeft, "A", seatNumber));
+		}
+		 	
 	}
 
 }
